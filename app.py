609847c9d0ae6814f579ac6bb905456b4e9a7084
rajiv-gowda <23711a04h3@necn.ac.in>
2026-06-21 20:35:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,13 @@ st.write("Model input shape:", model.input_shape)
 uploaded_file = st.file_uploader("Upload a .npy signal file", type=["npy"])
 if uploaded_file is not None:
     st.success("File uploaded successfully!")
+    if uploaded_file is not None:
+    st.success("File uploaded successfully!")
+
+    import numpy as np
+
+    signal = np.load(uploaded_file)
+
+    st.write("Signal shape:", signal.shape)
 
 st.success("Model loaded successfully!")
