6ae90ec39ecbbf7209a314c7d9ce4a5e9ea7b843
rajiv-gowda <23711a04h3@necn.ac.in>
2026-06-21 21:09:03 +0530

    Refactor file upload and prediction output handling

--- app.py
+++ app.py
@@ -10,23 +10,18 @@ def load_model():
 model = load_model()
 classes = ['8PSK', 'AM-DSB', 'AM-SSB', 'BPSK', 'CPFSK',
            'GFSK', 'PAM4', 'QAM16', 'QAM64', 'QPSK', 'WBFM']
-st.write("Model input shape:", model.input_shape)
 uploaded_file = st.file_uploader("Upload a .npy signal file", type=["npy"])
 
 if uploaded_file is not None:
-    st.success("File uploaded successfully!")
 
     import numpy as np
 
     signal = np.load(uploaded_file)
 
-    st.write("Signal shape:", signal.shape)
     signal = np.expand_dims(signal, axis=0)
 
 prediction = model.predict(signal)
 predicted_class = prediction.argmax(axis=1)[0]
 
 st.success(f"Predicted Modulation: {classes[predicted_class]}")
-st.write("Prediction shape:", prediction.shape)
 
-st.success("Model loaded successfully!")
