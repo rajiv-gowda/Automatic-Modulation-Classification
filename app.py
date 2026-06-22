93f64f647f42b52d8efa8ef4351ee60ddf8cfc2f
rajiv-gowda <23711a04h3@necn.ac.in>
2026-06-22 19:21:05 +0530

    Refactor success message and download button code

--- app.py
+++ app.py
@@ -157,25 +157,26 @@ if uploaded_file is not None:
         "Confidence (%)": [prediction[0][i] * 100 for i in top3_idx]
     })
 
-    st.success(
+        st.success(
         f"Predicted Modulation: {classes[predicted_class]} ({confidence:.2f}% confidence)"
     )
+
     pdf_buffer = create_pdf(
         classes[predicted_class],
         confidence,
         mean_amp,
         variance,
         peak_amp
-)
+    )
 
-st.download_button(
-    label="📄 Download Prediction Report (PDF)",
-    data=pdf_buffer,
-    file_name="amc_prediction_report.pdf",
-    mime="application/pdf"
-)
+    st.download_button(
+        label="📄 Download Prediction Report (PDF)",
+        data=pdf_buffer,
+        file_name="amc_prediction_report.pdf",
+        mime="application/pdf"
+    )
+
     st.subheader("Per-Class Accuracy")
-
     per_class_df = pd.read_csv("data/per_class_accuracy.csv")
 
     st.dataframe(
