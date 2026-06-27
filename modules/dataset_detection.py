6e0495d86b733a8df82cb81d8837ba368500d817
PANDI RAJIV <23711a04h3@necn.ac.in>
2026-06-27 13:05:44 +0530

    Update dataset_detection.py

--- modules/dataset_detection.py
+++ modules/dataset_detection.py
@@ -1,1 +1,32 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+def show_dataset_detection(model, classes):
+
+    st.title("📂 Dataset Detection")
+
+    st.info("Upload a .npy signal or use the sample signal.")
+
+    uploaded_file = st.file_uploader(
+        "Upload Signal",
+        type=["npy"]
+    )
+
+    use_sample = st.button("🚀 Use Sample Signal")
+
+    signal = None
+
+    if uploaded_file is not None:
+
+        signal = np.load(uploaded_file)
+
+    elif use_sample:
+
+        signal = np.load("sample_signal.npy")
+
+    if signal is None:
+        return
+
+    st.success("Signal loaded successfully.")
