6f7ecd10458e50d29712b7ad5ab910c17f0276ec
PANDI RAJIV <23711a04h3@necn.ac.in>
2026-06-29 08:21:44 +0530

    Update signal_generator.py

--- modules/signal_generator.py
+++ modules/signal_generator.py
@@ -4,22 +4,42 @@ import numpy as np
 class SignalGenerator:
     def __init__(self, num_symbols=128):
         self.num_symbols = num_symbols
-    def generate_bpsk(self):
-        """
-        Generate BPSK I/Q samples
-        """
+    def generate_bpsk(self, snr_db=20):
+    """
+    Generate RadioML-like BPSK signal
+    """
 
         bits = np.random.randint(0, 2, self.num_symbols)
 
         symbols = 2 * bits - 1
 
         I = symbols.astype(np.float32)
+        Q = np.zeros_like(I)
 
-        Q = np.zeros(self.num_symbols, dtype=np.float32)
+        signal = np.column_stack((I, Q))
 
-        iq = np.column_stack((I, Q))
+    # Random phase rotation
+        phase = np.random.uniform(0, 2 * np.pi)
 
-        return iq
+        rotation = np.array([
+            [np.cos(phase), -np.sin(phase)],
+            [np.sin(phase),  np.cos(phase)]
+        ])
+
+        signal = signal @ rotation.T
+
+    # Add AWGN Noise
+        signal_power = np.mean(signal ** 2)
+
+        snr_linear = 10 ** (snr_db / 10)
+
+        noise_power = signal_power / snr_linear
+
+        noise = np.sqrt(noise_power) * np.random.randn(*signal.shape)
+
+        signal += noise
+
+        return signal.astype(np.float32)
     def generate_qpsk(self):
         """
         Generate QPSK I/Q samples
